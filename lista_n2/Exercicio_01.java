df601d9ba60d8c3396dab966d21c811e38563c0e
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-11-13 19:40:10 -0300

    Add files via upload

--- lista_n2/Exercicio_01.java
+++ lista_n2/Exercicio_01.java
@@ -1,42 +1,43 @@
 /*
-Faça um programa que leia dois números inteiros, o primeiro é o valor inicial de um
-contador, e o segundo é o valor final do contador (testar se o valor inicial fornecido é
-inferior ao valor final). Usando o comando While, escreva na tela uma contagem que
-comece no primeiro número lido, escreva os números seguintes colocando sempre
-apenas um número em cada nova linha da tela, e terminando a contagem quando chegar
-ao valor final indicado.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package lista_N2;
 
-package lista_n2;
 import java.util.Scanner;
+
 public class Exercicio_01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        int  num , cont_1 = 0, cont_2 = 0 ;
-            //entrada de dados 
+        Scanner entrada = new Scanner(System.in);
+        int valor_inicio , cont_2,cont_1, valor_final;
+        
+        //entrada de dados 
         System.out.println();
         System.out.println("Informe o primeiro valor : ");
-        cont_1 = entrada.nextInt();
-        System.out.println("Informe o segundo valor : ");
-        cont_2 = entrada.nextInt();
-       //processamento de dados
-        while(cont_1 >= cont_2){
-            cont_1++;
-        }  
-           while(cont_1 <= cont_2){ 
-            if (cont_1 <= cont_2) {
+        valor_inicio = entrada.nextInt();
+        cont_1 = valor_inicio;
+        do {
+
+            System.out.println("Informe o segundo valor : ");
+            valor_final = entrada.nextInt();
+            cont_2 = valor_final;
+            //teste se o valor e menor que o primeiro 
+            if (cont_2 <= cont_1) {
+                System.out.println("=================================================");
+                System.out.println("o segundo valor deve ser maior que o primeiro ");
+            }
+        } while (cont_2 <= cont_1);
+        System.out.println("=================================================");
+        while (cont_1 <= cont_2) {
+           
                 
-                System.out.println("o valor inicial tem que ser inferior ao menor ");
-            }
-            else{
-                System.out.println("valor valido");
-            }
-           }
-            
-    
+            System.out.println(cont_1);
+            cont_1++;
+            System.out.println("=================================================");
+        }
+
     }
+
 }
