25e24c394b30999787f2b1820ceb4b4646fe1355
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2019-11-12 22:36:22 -0300

    Add files via upload

---
+++ lista_n2/Lista_n2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista_n2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Lista_n2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
